4f58ae826b6ce272d4b4c7b35a2ca2b7f0f9d5c4
tmm7an <julien.jamme@insee.fr>
2025-03-16 21:06:48 +0000

    ajout calcul betas pour ratio

---
+++ R/estimer_intervalle_confiance_ratio.R
@@ -0,0 +1,539 @@
+#' Calcule P(|R-R'|>beta) pour R= A/B et beta fixés.
+#'
+#' @param A numérateur
+#' @param B dénominateur
+#' @param D Déviation max de la CKM
+#' @param V Variance de la CKM
+#' @param js Seuil interdiction de la CKM
+#' @param ptab data.frame issu de ptable
+#' @param betas Seuil des écarts entre R et R'
+#' @param posterior Booléen: Si \code{TRUE}, c'est beta' qui est calculé.
+#'
+#' @return dataframe de deux colonnes (beta, proba)
+#'
+#' @details
+#' La fonction prend un seul ratio en entrée.
+#' Si \code{posterior=FALSE}, le calcul repose sur l'approche a priori,
+#' c'est-à-dire que le ratio fourni (A/B) est le ratio réel/original.
+#' Sinon, le calcul repose sur l'approche a posteriori,
+#' c'est-à-dire que le ratio fourni (A/B) est le ratio issu de la perturbation
+#' par CKM.
+#'
+#'
+#' @examples
+#' ptab <- ptable::create_cnt_ptable(D = 15, V = 30.1, js = 0)@pTable |> as.data.frame()
+#' # Calcul de alpha pour la stat originale = 10/15 pour différents beta
+#' calculer_proba_ecart_ratio(A=10,B=15,D = 15, V = 30.1, js = 0, ptab = ptab)
+#' # Calcul de alpha pour la stat perturbée = 10/15 pour différentes valeurs de beta'
+#' calculer_proba_ecart_ratio(A=10,B=15,D = 15, V = 30.1, js = 0, ptab = ptab, posterior = TRUE)
+#' @importFrom dplyr %>%
+#' @importFrom dplyr summarise
+#' @importFrom dplyr group_by
+#' @importFrom dplyr left_join
+#' @importFrom dplyr filter
+#' @importFrom dplyr arrange
+#' @importFrom dplyr tibble
+#'
+#' @export
+calculer_proba_ecart_ratio <- function(
+    A,
+    B,
+    D,
+    V,
+    js = 0,
+    ptab,
+    betas = c(0, 1, 5, 10, 20),
+    posterior = FALSE
+) {
+
+  if (posterior) {
+    if ((A <= js & A > 0) | A < 0 | (B <= js & B > 0) | B < 0) {
+      message(
+        "Les comptages A et B ne sont pas cohérents (négatifs ou inférieurs au seuil de sensibilité js)"
+      )
+      return(NULL)
+    } else{
+      probas_ecarts <- expand.grid(
+        Ap = A,
+        Bp = B,
+        zA = -D:D,
+        zB = -D:D,
+        stringsAsFactors = FALSE,
+        KEEP.OUT.ATTRS = FALSE
+      ) |>
+        as.data.frame() |>
+        mutate(
+          Rp = Ap / Bp * 100,
+          Ao = Ap - zA,
+          Bo = Bp - zB,
+          Ro = Ao / Bo * 100,
+          # Par convention l'écart relatif est passé à 1 si le dénominateur est nul
+          # Delta_R = ifelse(is.nan(Rp) | is.infinite(Rp), 1, abs(R - Rp)/R),
+          # Choix pour l'écart absolu: passé à R si le ratio perturbé est infini ou indéterminé
+          Delta_R = ifelse(
+            is.nan(Ro) |
+              is.infinite(Ro),
+            ifelse(is.nan(Rp) | is.infinite(Rp), 0, Rp),
+            abs(Ro - Rp)
+          ),
+          iA = ifelse(Ao > max(ptab$i), max(ptab$i), Ao),
+          iB = ifelse(Bo > max(ptab$i), max(ptab$i), Bo)
+        ) |>
+        mutate(pb = Ao < 0 |
+                 Bo < 0 |
+                 (Ap > 0 & Ao == 0) | (Bp > 0 & Bo == 0)) |>
+        dplyr::filter(!pb) |>
+        left_join(ptab |> select(
+          iA = i,
+          zA = v,
+          p_zA = p
+        ), by = c("iA", "zA")) |>
+        left_join(ptab |> select(
+          iB = i,
+          zB = v,
+          p_zB = p
+        ), by = c("iB", "zB")) |>
+        mutate(p_Ap_Bp = p_zA * p_zB) |>
+        group_by(Delta_R) |>
+        summarise(p_ecart = sum(p_Ap_Bp))
+    }
+  } else{
+    if (A < 0 | B < 0) {
+      message("Les comptages A et B ne sont pas cohérents (négatifs)")
+      return(NULL)
+    } else{
+      probas_ecarts <- expand.grid(
+        Ao = A,
+        Bo = B,
+        zA = -D:D,
+        zB = -D:D,
+        stringsAsFactors = FALSE,
+        KEEP.OUT.ATTRS = FALSE
+      ) |>
+        as.data.frame() |>
+        mutate(
+          Ro = Ao / Bo * 100,
+          Ap = Ao + zA,
+          Bp = Bo + zB,
+          Rp = Ap / Bp * 100,
+          # Par convention l'écart relatif est passé à 1 si le dénominateur est nul
+          # Delta_R = ifelse(is.nan(Rp) | is.infinite(Rp), 1, abs(R - Rp)/R),
+          # Choix pour l'écart absolu: passé à R si le ratio perturbé est infini ou indéterminé
+          Delta_R = ifelse(is.nan(Rp) |
+                             is.infinite(Rp), Ro, abs(Ro - Rp)),
+          iA = ifelse(Ao > max(ptab$i), max(ptab$i), Ao),
+          iB = ifelse(Bo > max(ptab$i), max(ptab$i), Bo)
+        ) |>
+        dplyr::filter(Ap >= 0 & Bp >= 0) %>%
+        {
+          if (js > 0)
+            dplyr::filter(., (Ap == 0 | Ap > js) & (Bp == 0 | Bp > js))
+          else
+            .
+        } %>%
+        left_join(ptab |> select(
+          iA = i,
+          zA = v,
+          p_zA = p
+        ), by = c("iA", "zA")) |>
+        left_join(ptab |> select(
+          iB = i,
+          zB = v,
+          p_zB = p
+        ), by = c("iB", "zB")) |>
+        mutate(p_Ap_Bp = p_zA * p_zB) |>
+        group_by(Delta_R) |>
+        summarise(p_ecart = sum(p_Ap_Bp))
+    }
+  }
+
+  betas <- if (!0 %in% betas) c(0, sort(betas)) else sort(betas)
+
+  res <- purrr::map(betas, \(beta) {
+    probas_ecarts |>
+      mutate(Sup_beta = Delta_R >= beta) |>
+      group_by(Sup_beta) |>
+      summarise(p_delta_sup_beta = sum(p_ecart)) |>
+      dplyr::filter(Sup_beta) |>
+      mutate(beta = beta) |>
+      select(beta, p_delta_sup_beta)
+  }) |>
+    purrr::list_rbind() |>
+    full_join(tibble(beta = betas), by = "beta") |>
+    mutate(p_delta_sup_beta = ifelse(is.na(p_delta_sup_beta), 0, p_delta_sup_beta)) |>
+    #normalisation
+    mutate(p_delta_sup_beta = p_delta_sup_beta / p_delta_sup_beta[beta == 0]) |>
+    arrange(beta)
+
+  return(res |> select(beta, proba = p_delta_sup_beta))
+
+}
+
+
+#' Calcule P(|R-R'|>beta) pour chaque R=A/B d'un dataframe donné et pour chaque beta fixé.
+#'
+#' @param data data.frame à 2 colonnes, le numérateur et le dénominateur
+#' de chaque ratio
+#' @inheritParams calculer_proba_ecart_ratio
+#' @param parallel Booléen, si le calcul doit être parallélisé
+#' @param max_cores, integer, nombre maximal de travaux à réaliser en parallèle
+#'
+#' @return un dataframe
+#' @details
+#' Si \code{posterior=FALSE}, le calcul repose sur l'approche a priori,
+#' c'est-à-dire que le ratio fourni (A/B) est le ratio réel/original.
+#' Sinon, le calcul repose sur l'approche a posteriori,
+#' c'est-à-dire que le ratio fourni (A/B) est le ratio issu de la perturbation
+#' par CKM.
+#'
+#' @examples
+#' test <- data.frame(
+#'   A = sample(1:50, 10, replace = TRUE),
+#'   B = sample(50:1000, 10, replace = TRUE)
+#' )
+#'
+#' D = 10
+#' V = 10
+#' js = 4
+#'
+#' # Approche a priori
+#' res <- calculer_proba_ecart_ratio_sur_df(test, D, V)
+#'
+#' # Approche a posteriori
+#' res_ap <- calculer_proba_ecart_ratio_sur_df(test, D, V, posterior = TRUE)
+#' @importFrom dplyr %>%
+#' @export
+calculer_proba_ecart_ratio_sur_df <- function(
+    data,
+    D,
+    V,
+    js = 0,
+    betas = c(0, 1, 2, 5, 10, 20),
+    posterior = FALSE,
+    parallel = FALSE,
+    max_cores = NULL
+) {
+
+  ptab <- ptable::create_cnt_ptable(D = D, V = V, js = js)@pTable |> as.data.frame()
+
+  data_f <- unique(data)
+  print(paste0("Doublons supprimés: ", nrow(data) - nrow(data_f)))
+
+  names(data_f) <- c("num", "denom")
+
+  oplan <- future::plan(future::sequential)
+  on.exit(future::plan(oplan))
+
+  if (parallel) {
+    n_cores <- as.numeric(future::availableCores())
+    util_cores <- if (is.null(max_cores))
+      n_cores - 1
+    else if (max_cores >= n_cores)
+      n_cores - 1
+    else
+      max_cores
+
+    future::plan(future::multisession, workers = util_cores)
+    res <- furrr::future_map2(
+      data_f$num,
+      data_f$denom,
+      \(n, d) calculer_proba_ecart_ratio(n, d, D, V, js, ptab, betas, posterior) |>
+        mutate(A = n, B = d, R = n / d * 100),
+      .progress = TRUE
+    ) |>
+      purrr::list_rbind()
+    #mutate(p_delta_infeg_beta = 1 - p_delta_sup_beta)
+
+    future::plan(future::sequential)
+  } else{
+    res <- purrr::map2(
+      data_f$num,
+      data_f$denom,
+      \(n, d) calculer_proba_ecart_ratio(n, d, D, V, js, ptab, betas, posterior) |>
+        mutate(A = n, B = d, R = n / d * 100),
+      .progress = TRUE
+    ) |>
+      purrr::list_rbind()
+    # mutate(p_delta_infeg_beta = 1 - p_delta_sup_beta)
+  }
+
+  return(res |> select(beta, A, B, R, proba))
+}
+
+
+#' Estime le beta minimal tel que P(|R-R'|>beta) < alpha, pour alpha fixé et
+#' pour un ratio R=A/B fourni en entrée.
+#'
+#' @inheritParams calculer_proba_ecart_ratio
+#' @param beta_min integer, seuil de précision minimal
+#' @param beta_max integer, seuil de précision maximal
+#' @param precision double, niveau de précision de l'estimation de beta
+#' @param alpha double, niveau d'erreur fixé
+#'
+#' @return numeric value
+#' @details
+#' Si \code{posterior=FALSE}, le calcul repose sur l'approche a priori,
+#' c'est-à-dire que le ratio fourni (A/B) est le ratio réel/original.
+#' Sinon, le calcul repose sur l'approche a posteriori,
+#' c'est-à-dire que le ratio fourni (A/B) est le ratio issu de la perturbation
+#' par CKM.
+#'
+#' Le meilleur beta est recherché dans l'intervalle [beta_min; beta_max].
+#'
+#' @examples
+#' library(dplyr)
+#' ptab <- ptable::create_cnt_ptable(D = 15, V = 30.1, js = 0)@pTable |> as.data.frame()
+#' r1 <- calculer_proba_ecart_ratio(A=100,B=1500,D = 15, V = 30.1, js = 0, ptab = ptab, betas = seq(0,10,0.1))
+#' r1 |> filter(proba <= 0.05) |> head(1) |> pull(beta)
+#' calculer_beta_ratio(A=100,B=1500,D = 15, V = 30.1, js = 0, ptab = ptab)
+#' r2 <- calculer_proba_ecart_ratio(A=100,B=1500,D = 15, V = 30.1, js = 0, ptab = ptab, betas = seq(0,10,0.1), posterior = TRUE)
+#' r2 |> filter(proba <= 0.05) |> head(1) |> pull(beta)
+#' calculer_beta_ratio(A=100,B=1500,D = 15, V = 30.1, js = 0, ptab = ptab, posterior = TRUE)
+#' @importFrom dplyr pull
+#' @importFrom dplyr filter
+#' @importFrom dplyr %>%
+#' @export
+calculer_beta_ratio <- function(
+    A,
+    B,
+    D,
+    V,
+    js = 0,
+    ptab,
+    beta_min = 0,
+    beta_max = 10,
+    precision = 0.1,
+    alpha = 0.05,
+    posterior = FALSE
+) {
+
+  betas = seq(beta_min, beta_max, by = precision)
+  ns = length(betas)
+  smin = betas[1]
+  smax = betas[ns]
+  smid = betas[ifelse(ns %% 2 == 0, ns / 2, (ns + 1) / 2)]
+
+  probas <- calculer_proba_ecart_ratio(
+    A,
+    B,
+    D,
+    V,
+    js = js,
+    ptab,
+    betas = c(smin, smax),
+    posterior = posterior
+  )
+  proba_min <- probas |> dplyr::filter(beta == smin) |> pull(proba)
+  proba_max <- probas |> dplyr::filter(beta == smax) |> pull(proba)
+  ecart = smax - smin
+
+  if (round(ecart - precision, 8) > 0) {
+    if (proba_min <= alpha) {
+      #dans ce cas on atteint l'objectif avec le seuil min
+      return(probas$beta[1])
+    } else if (proba_max >= alpha) {
+      #dans ce cas on atteindra jamais l'objectif même avec seuil max
+      return(probas$beta[2])
+    } else{
+      #cas général où le beta est entre les deux
+      proba_mid <- calculer_proba_ecart_ratio(
+        A,
+        B,
+        D,
+        V,
+        js = js,
+        ptab,
+        betas = smid,
+        posterior = posterior
+      ) |>
+        dplyr::filter(beta == smid) |>
+        pull(proba)
+      if (proba_mid > alpha) {
+        return(
+          calculer_beta_ratio(
+            A,
+            B,
+            D,
+            V,
+            js,
+            ptab,
+            beta_min = smid,
+            beta_max = smax,
+            precision = precision,
+            alpha = alpha,
+            posterior = posterior
+          )
+        )
+      } else{
+        return(
+          calculer_beta_ratio(
+            A,
+            B,
+            D,
+            V,
+            js,
+            ptab,
+            beta_min = smin,
+            beta_max = smid,
+            precision = precision,
+            alpha = alpha,
+            posterior = posterior
+          )
+        )
+      }
+    }
+  } else{
+    return(probas |> dplyr::filter(proba <= min(proba)) |> pull(beta))
+  }
+
+}
+
+
+
+
+#' Estime le beta minimal tel que P(|R-R'|>beta) < alpha, pour alpha fixé et
+#' pour chacun des ratio R=A/B fournis dans le dataframe en entrée.
+#'
+
+#'
+#' Détermine la meilleure précision qu'il est possible d'atteindre dans le cas
+#' du calcul d'un ratio. La fonction prend en entrée un dataframe et calcule
+#' la précision pour chacun des ratios du dataframe.
+#'
+#' Si posterior = FALSE: détermine beta0 pour alpha fixé
+#' Si posterior = TRUE: détermine beta0' pour alpha fixé
+#'
+#' @param data dataframe avec deux colonnes correspondant aux numérateurs et dénominateurs des ratios
+#' @inheritParams calculer_beta_ratio
+#' @param parallel Booléen, si le calcul doit être parallélisé
+#' @param max_cores, integer, nombre maximal de travaux à réaliser en parallèle
+#'
+#' @return data.frame avec les colonnes suivantes:
+#' - A = numérateur
+#' - B = dénominateur
+#' - R = ratio = A/B*100
+#' - alpha
+#' - beta = correspondant à beta0 si \code{posterior=FALSE} ou
+#' à beta0' si \code{posterior=TRUE}
+#'
+#' @details
+#' Si \code{posterior=FALSE}, le calcul repose sur l'approche a priori,
+#' c'est-à-dire que le ratio fourni (A/B) est le ratio réel/original.
+#' Sinon, le calcul repose sur l'approche a posteriori,
+#' c'est-à-dire que le ratio fourni (A/B) est le ratio issu de la perturbation
+#' par CKM.
+#'
+#' Le meilleur beta est recherché dans l'intervalle [beta_min; beta_max].
+#'
+#' @examples
+#' test <- data.frame(
+#'   A = sort(rep(c(1,50,100),4)),
+#'   B = rep(c(100,200,300,500), 3)
+#' )
+#'
+#' D = 5
+#' V = 1
+#'
+#' # Approche a priori
+#' res <- calculer_beta_ratio_sur_df(test, D, V)
+#' # Approche a posteriori
+#' res_ap <- calculer_beta_ratio_sur_df(test, D, V, posterior = TRUE)
+#' @importFrom future plan
+#' @importFrom future sequential
+#' @importFrom future availableCores
+#' @importFrom future multisession
+#' @importFrom furrr future_map2
+#' @importFrom purrr list_rbind
+#' @importFrom dplyr tibble
+#' @export
+calculer_beta_ratio_sur_df <- function(
+    data,
+    D,
+    V,
+    js = 0,
+    beta_min = 0,
+    beta_max = 10,
+    precision = 0.1,
+    alpha = 0.05,
+    posterior = FALSE,
+    parallel = FALSE,
+    max_cores = NULL
+) {
+
+  ptab <- ptable::create_cnt_ptable(D = D, V = V, js = js)@pTable |> as.data.frame()
+
+  data_f <- unique(data)
+  print(paste0("Doublons supprimés: ", nrow(data) - nrow(data_f)))
+
+  names(data_f) <- c("num", "denom")
+
+  oplan <- future::plan(future::sequential)
+  on.exit(future::plan(oplan))
+
+  if (parallel) {
+    n_cores <- as.numeric(future::availableCores())
+    util_cores <- if (is.null(max_cores))
+      n_cores - 1
+    else if (max_cores >= n_cores)
+      n_cores - 1
+    else
+      max_cores
+
+    future::plan(future::multisession, workers = util_cores)
+    res <- furrr::future_map2(
+      data_f$num,
+      data_f$denom,
+      \(n, d) tibble(
+        A = n,
+        B = d,
+        R = n / d * 100,
+        alpha = alpha,
+        beta = calculer_beta_ratio(
+          n,
+          d,
+          D,
+          V,
+          js,
+          ptab,
+          beta_min,
+          beta_max,
+          precision,
+          alpha,
+          posterior = posterior
+        )
+      ),
+      .progress = TRUE
+    ) |>
+      purrr::list_rbind()
+
+    future::plan(future::sequential)
+  } else{
+    res <- purrr::map2(
+      data_f$num,
+      data_f$denom,
+      \(n, d) tibble(
+        A = n,
+        B = d,
+        R = n / d * 100,
+        alpha = alpha,
+        beta = calculer_beta_ratio(
+          n,
+          d,
+          D,
+          V,
+          js,
+          ptab,
+          beta_min,
+          beta_max,
+          precision,
+          alpha,
+          posterior = posterior
+        )
+      ),
+      .progress = TRUE
+    ) |>
+      purrr::list_rbind()
+  }
+
+  return(res)
+}
--- R/globals.R
+++ R/globals.R
@@ -1,5 +1,8 @@
 #' @import utils
 utils::globalVariables(
   c("ck_end", "v","res_ckm","p_int_ub","p_int_lb","i","nb_obs","n","rkey",
-    "p", "X", "Xp", "ckey", "rkey_tot", ".")
+    "p", "X", "Xp", "ckey", "rkey_tot", ".",
+    "A", "B", "Ap", "Bp", "R", "Ro", "Rp", "Ao", "Bo", "zA", "zB", "pb",
+    "p_zA", "p_zB", "Delta_R", "p_Ap_Bp", "Sup_beta", "p_ecart",
+    "p_delta_sup_beta", "p_delta_sup_beta", "proba")
 )
